424a7684a4dd814ac350bd811d34e30608f9dfd9
chazzlee <chazz@dev.com>
2022-10-13 15:18:23 -0400

    reformat models

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,28 +10,24 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+  has_many :enrollments,
+           primary_key: :id,
+           foreign_key: :course_id,
+           class_name: :Enrollment
 
-    has_many :enrollments,
-        primary_key: :id,
-        foreign_key: :course_id,
-        class_name: :Enrollment
+  has_many :enrolled_students,
+           through: :enrollments,
+           source: :student
 
-    has_many :enrolled_students,
-        through: :enrollments,
-        source: :student
+  belongs_to :prerequisite,
+             primary_key: :id,
+             foreign_key: :prereq_id,
+             class_name: :Course,
+             optional: true
 
-    belongs_to :prerequisite,
-        primary_key: :id,
-        foreign_key: :prereq_id,
-        class_name: :Course,
-        optional: true
-
-    belongs_to :instructor,
-        primary_key: :id,
-        foreign_key: :instructor_id,
-        class_name: :User,
-        optional: true
-
-
-    
+  belongs_to :instructor,
+             primary_key: :id,
+             foreign_key: :instructor_id,
+             class_name: :User,
+             optional: true
 end
