cd026dc003b85225802f15f8b808899851a9639e
rtexelm <texelist@gmail.com>
2022-10-13 15:01:43 -0400

    Added associations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
+    
 end
